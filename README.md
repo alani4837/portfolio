c184b9b389ba47c73cc10c49cf6d600863e87103
alani4837 <chrisbounas4837@hotmail.com>
2018-05-07 22:47:27 +0300

    Added a portfolio.

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## Setup
+
+```console
+$ npm i
+$ npm run build
+$ npm run serve
+```
+
+Go to http://localhost:5000 to see the generated site.
---
+++ site.config.js
@@ -0,0 +1,9 @@
+const projects = require('./src/data/projects')
+
+module.exports = {
+  site: {
+    title: 'User52',
+    description: 'Personal Website',
+    projects
+  }
+}
